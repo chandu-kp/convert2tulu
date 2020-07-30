13759ef8e3b8d3aa8b248b2e983eb787c7fdac74
chandrahasa <simplymangalore@gmail.com>
2020-07-30 19:42:56 +1000

    Updated to fix ರ್ issue

--- js/converter.js
+++ js/converter.js
@@ -24,7 +24,7 @@ function convertWithEscape(txt, escapeChar) {
 
         // fix for chillu letters between kannada and malayalam letters
         if ((letterToConvert in k_m_virama_mapping) && doMap === true) {
-            if (txt[i - 1] in k_m_chillu_latters_mapping) {
+            if ((txt[i - 1] in k_m_chillu_latters_mapping) && isNotEndOfWord(txt, i)) {
                 //remove the letter and replace with corresponding chillu letter 
                 convertedWord.pop();
                 letterToConvert = k_m_chillu_latters_mapping[txt[i - 1]];
@@ -38,4 +38,11 @@ function convertWithEscape(txt, escapeChar) {
         }
     }
     return convertedWord.join('');
+}
+
+function isNotEndOfWord(txt, currentIndex) {
+    return (txt[currentIndex+1] != ' ' 
+            && txt[currentIndex+1] != '\n'
+            && txt[currentIndex+1] != '\r'
+            && txt[currentIndex+1] != '\r\n');
 }
